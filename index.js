919910956decb6233930d505d8d44e0bb1eef51a
Yassine <elbayass59@gmail.com>
2017-01-07 15:36:50 +0100

    projet_piano

---
+++ index.js
@@ -0,0 +1,19 @@
+function myfunction(){
+    return son1.play();
+  }
+  function myfunction1(){
+    return son2.play();
+  }
+  function myfunction2(){
+    return son3.play();
+  }
+  function myfunction3(){
+    return son4.play();
+  }
+  function myfunction4(){
+    return son5.play();
+  }
+
+  function myfunction5(){
+    return son6.play();
+  }
